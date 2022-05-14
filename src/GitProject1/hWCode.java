969140647650eb9c8a890877d4a241e7dcd21dc3
kishanshah16091 <kishan.shah16091@gmail.com>
2022-05-14 15:40:17 -0400

    Homework code

---
+++ src/GitProject1/hWCode.java
@@ -0,0 +1,11 @@
+package GitProject1;
+
+public class hWCode {
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i++) {
+
+            System.out.println(i);
+
+        }
+    }
+}
