4ac0ea8b914d45d75f6b3ae5d5442def8fe38567
kishanshah16091 <kishan.shah16091@gmail.com>
2022-05-14 12:02:21 -0400

    second commit

--- src/GitProject1/GitClass01.java
+++ src/GitProject1/GitClass01.java
@@ -3,6 +3,7 @@ package GitProject1;
 public class GitClass01 {
     public static void main(String[] args) {
         System.out.println("It's my first Git class");
-
+        System.out.println("It's my second Git class");
+        System.out.println("It's my third Git class");
     }
 }
